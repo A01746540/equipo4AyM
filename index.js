52aba41a1284a09b0be209494ab48535ded67fdd
EC2 Default User <ec2-user@ip-172-31-90-8.ec2.internal>
2021-09-30 22:11:08 +0000

    Se corrigiegon errores gramaticales y se asigno el puerto 8084

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express")
 const app = express();
 
 app.get("/prueba", (req, res) => {
-    res.send("Prueba de servidor funcionando")
+    res.send("Prueba de servidor funcionando - Equipo 4 Alexis y Maxine")
 })
 
-app.listen(8080, () => {
-    console.log("Servidr en linea por el puerto 8080")
+app.listen(8084, () => {
+    console.log("Servidor en linea por el puerto 8084")
 })
