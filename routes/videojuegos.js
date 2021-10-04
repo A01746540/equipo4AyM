43d1f321970a19849b24cd061db743a31ffc77d3
Edgar Alexis González Amador <A01746540@itesm.mx>
2021-10-04 16:48:48 -0500

    Actualización de código

--- routes/videojuegos.js
+++ routes/videojuegos.js
@@ -3,13 +3,16 @@ const vjController = require('../controllers/videojuegos')
 const router = express.Router()
 
 
-//Formulario de ingreso de datos
-router.get('/agregarVideojuego', vjController.getAgregarVideojuego)
-    //Servicio para el procesamiento de los datos
+//Formulario de ingreso de datos (front-end) 
+//router.get('/agregarVideojuego',vjController.getAgregarVideojuego)
+//Servicio para el procesamiento de los datos
 router.post('/agregarVideojuego', vjController.postAgregarVideojuego)
-    //Pagina de confirmacion
-router.get('/confirmacionDatos', vjController.getConfirmacionDatos)
+    //Pagina de confirmacion (front-end)
+    //router.get('/confirmacionDatos',vjController.getConfirmacionDatos)
     //Visualización de los datos guardados
-router.get('/mostrarVideojuegos', vjController.getMostrarVideojuegos)
+router.get('/obtenerVideojuegos', vjController.getObtenerVideojuegos)
+router.post('/borrarVideojuego', vjController.postBorrarVideojuego)
+router.post('/actualizarVideojuego', vjController.postActualizarVideojuego)
+
 
 module.exports = router
